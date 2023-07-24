5bedfdfdec255bb1effd0c9da88f54783ffcd437
runru1030 <runru1030@gmail.com>
2023-07-24 14:15:12 +0900

    chore : 채팅 스크롤

--- src/app/chat/components/ChatSection.tsx
+++ src/app/chat/components/ChatSection.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "next/navigation";
 import ChatInput from "./ChatInput";
 import useFirebaseMessage from "./hooks/useFirebaseMessage";
 import { Message } from "../types";
+import { useEffect, useRef } from "react";
 
 interface MessageProps {
   message: Message;
@@ -13,28 +14,34 @@ const Message = ({ message, isMine }: MessageProps) => {
     <div
       className={clsx(
         isMine ? "self-end bg-gray-200" : "self-start border border-gray-200",
-        "py-2 px-4 rounded-3xl"
+        "py-2 px-4 rounded-3xl first:mt-auto"
       )}
     >
+      {message.photoURL && <img src={message.photoURL} className="" />}
       {message.text}
     </div>
   );
 };
 const ChatSection = () => {
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
   const searchParams = useSearchParams();
   const channelID = searchParams?.get("channel") || "";
 
   const userId = "test";
   const { messages } = useFirebaseMessage(channelID);
 
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div
       className={clsx(
         channelID === "" ? "hidden" : "visible",
-        "flex-1 flex flex-col gap-8 p-4"
+        "flex-1 flex flex-col py-4"
       )}
     >
-      <div className="flex flex-col justify-end flex-1 gap-2">
+      <div className="flex flex-col flex-1 gap-2 overflow-y-scroll px-4">
         {messages.map((message) => (
           <Message
             key={message.id}
@@ -42,6 +49,7 @@ const ChatSection = () => {
             isMine={message.userId === userId}
           />
         ))}
+        <div ref={messageEndRef}></div>
       </div>
       <ChatInput {...{ userId, channelID }} />
     </div>
