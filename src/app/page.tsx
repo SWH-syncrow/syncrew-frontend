e705daef4d228a8b167027747fc88e55d3a1a02d
runru1030 <runru1030@gmail.com>
2023-07-26 17:04:15 +0900

    feat : 헤더 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import PageContent from "./PageContent";
+import Header from "./components/Header/Header";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center">
+      <Header />
       <PageContent />
     </div>
   );
