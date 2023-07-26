95c958e59ebc361303c5d46eb47c2be67e2ee6c5
runru1030 <runru1030@gmail.com>
2023-07-27 01:48:29 +0900

    chore : 로그인 로직 추가

--- src/components/serverAuth.ts
+++ src/components/serverAuth.ts
@@ -5,3 +5,6 @@ import { cookies } from "next/dist/client/components/headers";
 export async function setRefreshTokenToCookie(refresh_token: string) {
   cookies().set("refresh_token", refresh_token);
 }
+export async function getRefreshTokenFromCookie() {
+  return cookies().get("refresh_token");
+}
