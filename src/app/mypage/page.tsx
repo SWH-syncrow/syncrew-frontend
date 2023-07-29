649669ac49853a1e0fcd68c3e45b128ee0e3b9cd
runru1030 <runru1030@gmail.com>
2023-07-30 01:44:43 +0900

    feat : 마이페이지 레이아웃

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import PageContent from "./PageContent";
+
+const Page = () => {
+  return (
+    <div className="w-full flex flex-col items-cente">
+      <PageContent />
+    </div>
+  );
+};
+
+export default Page;
