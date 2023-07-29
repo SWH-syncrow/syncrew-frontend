9524674b5d5cc86772a511009fbc5eace2ae2621
runru1030 <runru1030@gmail.com>
2023-07-30 00:15:00 +0900

    design : 로그인 페이지 퍼블리싱

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 import useAuthKakao from "@components/hooks/useAuthKakao";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import Kakao from "public/assets/kakao.svg";
+import Prev from "public/assets/icons/left.svg";
 
 const Page = () => {
   useAuthKakao();
@@ -9,18 +11,19 @@ const Page = () => {
 
   return (
     <div className="flex flex-col items-center  w-full h-screen">
-      <div className="w-[500px] h-full flex flex-col my-[70px] text-center">
+      <div className="w-[300px] h-full flex flex-col my-[70px] text-center">
         <button className="self-start" onClick={() => router.back()}>
-          이전
+          <Prev />
         </button>
-        <div className="flex flex-col items-center flex-1 py-20">
-          <span className="text-3xl font-semibold mb-20">
+        <div className="flex flex-col items-center flex-1 py-20 leading-[33px]">
+          <span className="text-[24px] font-semibold mb-20">
             디지털 정보 친구를
             <br /> 만나러 가볼까요?
           </span>
-          <span className="text-sm mb-4">먼저 로그인이 필요해요 :)</span>
+          <span className="text-sm mb-4 text-grey-200">
+            먼저 로그인이 필요해요 :)
+          </span>
           <Link
-            className="font-semibold bg-grey-300 w-full py-2"
             href={`https://kauth.kakao.com/oauth/authorize?client_id=${
               process.env.NEXT_PUBLIC_KAKAO_REST_KEY
             }&redirect_uri=${
@@ -29,10 +32,10 @@ const Page = () => {
                 : process.env.NEXT_PUBLIC_DOMAIN
             }/login&response_type=code`}
           >
-            카카오로 3초 만에 시작하기
+            <Kakao />
           </Link>
         </div>
-        <div className="border-t border-grey-300 text-grey-500 pt-4">
+        <div className="border-t border-grey-100 text-grey-200 pt-4 text-sm whitespace-nowrap">
           회원가입 시 syncrew의
           <br /> 서비스 이용 약관과 개인정보 보호정책에 동의하게 됩니다
         </div>
