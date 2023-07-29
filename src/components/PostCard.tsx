e4e13f6d446b2653b4a56a4e6d8303a044690ed4
runru1030 <runru1030@gmail.com>
2023-07-30 01:43:50 +0900

    chore : postcard 컴포넌트 전역이동 및 타입별 버튼

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,15 +1,52 @@
 import React, { useState } from "react";
-import { Post } from "../types";
+import { Post } from "../app/group/types";
 import { Button } from "src/components/Button";
 import clsx from "clsx";
 import Modal from "src/components/Modal";
 import Vector from "public/assets/icons/Vector.svg";
 import Request from "public/assets/icons/친구신청.svg";
+import Requested from "public/assets/icons/친구_신청_완료.svg";
+import Delete from "public/assets/icons/Delete.svg";
 import Down from "public/assets/icons/down_sm.svg";
 
-const PostCard = ({ id, username, temp, title, content }: Post) => {
+interface PostCardProps {
+  post: Post;
+  type?: "MINE" | "REQUESTED" | "NORMAL";
+}
+const PostCard = ({
+  post: { id, username, temp, title, content },
+  type = "NORMAL",
+}: PostCardProps) => {
   const [isFullView, setIsFullView] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+
+  const ButtonByType = () => {
+    switch (type) {
+      case "MINE":
+        return (
+          <Button className="mr-9 !p-0">
+            <Delete />
+          </Button>
+        );
+      case "REQUESTED":
+        return (
+          <div className="btn-orange flex items-center gap-1 font-medium h-9 mr-9 cursor-default">
+            <Requested />
+            친구 신청 완료
+          </div>
+        );
+      case "NORMAL":
+        return (
+          <Button
+            onClick={() => setOpenModal(true)}
+            className="btn-orange flex items-center gap-1 font-medium h-9 w-[126px] mr-9"
+          >
+            <Request />
+            친구 신청
+          </Button>
+        );
+    }
+  };
   return (
     <>
       <Modal open={openModal} onOpenChange={setOpenModal}>
@@ -29,13 +66,7 @@ const PostCard = ({ id, username, temp, title, content }: Post) => {
               {temp}˚C
             </span>
           </div>
-          <button
-            onClick={() => setOpenModal(true)}
-            className="btn-orange flex items-center gap-1 font-medium h-9 w-[126px] mr-9"
-          >
-            <Request />
-            친구 신청
-          </button>
+          <ButtonByType />
         </div>
         <div className="relative flex gap-12 justify-between w-[816px] py-9 px-10 shadow-normal rounded-2xl bg-white">
           <div className="flex flex-col gap-[15px] min-w-0">
