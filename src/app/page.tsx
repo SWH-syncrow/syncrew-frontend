bb7775cfdded5325b978ea80b5d047e6e0e39082
runru1030 <runru1030@gmail.com>
2023-07-31 15:13:14 +0900

    fix : msw 세팅 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import PageContent from "./PageContent";
 import Header from "./components/Header/Header";
 
-if (process.env.NODE_ENV === "development") {
-  import("../mocks");
-}
-
 const Home = () => {
-  return (
+  const [shouldRender, setShouldRender] = useState(
+    !(process.env.NODE_ENV === "development")
+  );
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      import("../mocks").then(async ({ initMocks }) => {
+        await initMocks();
+        setShouldRender(true);
+      });
+    }
+  }, []);
+
+  return shouldRender ? (
     <div className="w-full flex flex-col items-center">
       <Header />
       <PageContent />
     </div>
+  ) : (
+    <div>loading mocks</div>
   );
 };
 
