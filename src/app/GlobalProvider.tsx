3111e15ba6e132f12b40f43ac71da3388c909b7d
runru1030 <runru1030@gmail.com>
2023-07-31 15:49:19 +0900

    feat : 사용자 이전 url 추적

--- src/app/GlobalProvider.tsx
+++ src/app/GlobalProvider.tsx
@@ -2,8 +2,9 @@
 
 import useAuth from "@components/hooks/useAuth";
 import { atomWithReset } from "jotai/utils";
-import React from "react";
+import React, { useEffect } from "react";
 import { User } from "./types";
+import { usePathname } from "next/navigation";
 
 export const userAtom = atomWithReset<User>({
   id: -1,
@@ -15,10 +16,17 @@ export const userAtom = atomWithReset<User>({
 export const isLoggedInAtom = atomWithReset<boolean>(false);
 export default function GlobalProvider(props: { children: React.ReactNode }) {
   useAuth();
-  return (
-    <>
-      {/* <DevTools /> */}
-      {props.children}
-    </>
-  );
+  const path = usePathname();
+
+  const storePathValues = () => {
+    const storage = globalThis?.sessionStorage;
+    if (!storage) return;
+    const prevPath = storage.getItem("currentPath") || "";
+    storage.setItem("prevPath", prevPath);
+    storage.setItem("currentPath", globalThis.location.pathname);
+  };
+
+  useEffect(() => storePathValues, [path]);
+
+  return <>{props.children}</>;
 }
