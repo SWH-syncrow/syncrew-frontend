f9c3d49cca40b206f2363a51aae0ee117fbf7974
runru1030 <runru1030@gmail.com>
2023-08-01 22:59:52 +0900

    fix : order by createdat

--- src/app/chat/components/hooks/useFirebaseChannel.ts
+++ src/app/chat/components/hooks/useFirebaseChannel.ts
@@ -48,32 +48,32 @@ export const useGetChannels = () => {
         const channelsOfUser = querySnapshot.data()?.channels;
         const channelsQuery = query(
           collection(db, "channel"),
-          where(documentId(), "in", channelsOfUser),
-          orderBy(documentId())
-          // orderBy("created_at")
+          where(documentId(), "in", channelsOfUser)
         );
 
         const chUnsb = onSnapshot(channelsQuery, async (querySnapshot) => {
-          const resPromises = querySnapshot.docs.map(async (channel) => {
-            const notMe = await getDocs(
-              query(
-                collection(channel.ref, "users"),
-                where(documentId(), "!=", user.id.toString())
-              )
-            );
-            const chatUser = {
-              ...notMe.docs[0].data(),
-              id: notMe.docs[0].id,
-            };
-            return {
-              ...channel.data(),
-              id: channel.id,
-              chatUser,
-              isUnread:
-                channel.data().lastChatAt >
-                channel.data()?.lastVisitedAt?.[user.id],
-            };
-          });
+          const resPromises = querySnapshot.docs
+            .sort((a, b) => b.data().createdAt - a.data().createdAt)
+            .map(async (channel) => {
+              const notMe = await getDocs(
+                query(
+                  collection(channel.ref, "users"),
+                  where(documentId(), "!=", user.id.toString())
+                )
+              );
+              const chatUser = {
+                ...notMe.docs[0].data(),
+                id: notMe.docs[0].id,
+              };
+              return {
+                ...channel.data(),
+                id: channel.id,
+                chatUser,
+                isUnread:
+                  channel.data().lastChatAt >
+                  channel.data()?.lastVisitedAt?.[user.id],
+              };
+            });
           const res = (await Promise.all(resPromises)) as Channel[];
           setChannels(
             res.reduce(
