e66ad4f5afabb7b363df6bcf0e66260ad8de2a71
runru1030 <runru1030@gmail.com>
2023-08-02 20:25:28 +0900

    feat : 그룹 아이디 유효성 검사

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get("id");
+  const idRgx = /[^0-9]/;
+  if (id && (idRgx.test(id) || parseInt(id) > 12 || parseInt(id) < 1)) {
+    return NextResponse.redirect(new URL("/404", req.url));
+  }
+}
+
+export const config = {
+  matcher: "/group/:path*",
+};
