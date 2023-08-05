02cd795ce65324fed6a7e647d6e17c1f495b0d0b
runru1030 <runru1030@gmail.com>
2023-08-06 01:18:06 +0900

    chore : 채팅 이미지 next image

--- src/app/chat/_components/ChatSection/Message.tsx
+++ src/app/chat/_components/ChatSection/Message.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { Message } from "../types";
+import Image from "next/image";
 
 const Message = ({
   message: { photoURL, text },
@@ -17,9 +18,11 @@ const Message = ({
     >
       {photoURL && (
         <div className="relative max-w-[400px] w-fit rounded-3xl overflow-hidden">
-          <img
+          <Image
             src={photoURL}
             alt="첨부 이미지"
+            width={300}
+            height={300}
             className="relative object-contain max-h-[50vh] !z-0"
           />
           <div className="absolute top-0 w-full h-full bg-grey-50 !-z-10"></div>
