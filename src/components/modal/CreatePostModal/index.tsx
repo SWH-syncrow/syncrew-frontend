30700d34b17a91907d414a0f8b12cae4678a7cbf
runru1030 <runru1030@gmail.com>
2023-08-05 13:48:00 +0900

    fix : 신청글 작성 모달 초기화

--- src/components/modal/CreatePostModal/index.tsx
+++ src/components/modal/CreatePostModal/index.tsx
@@ -31,6 +31,11 @@ const CreatePostModal = ({ groupId, groupName }: CreatePostModalProps) => {
       await PostApis.createPost(post),
     onSuccess: () => {
       setOpenAtom(false);
+      setPost({
+        title: "",
+        content: "",
+        groupId: parseInt(groupId),
+      });
       queryClient.invalidateQueries(["getGroupPosts"]);
     },
     onError: (e) => {
