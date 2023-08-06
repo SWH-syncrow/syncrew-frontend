5c766a5861b22904d672b98f86538a979cb04a5f
runru1030 <runru1030@gmail.com>
2023-08-06 19:42:43 +0900

    chore : query 추가 설정

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -2,13 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useAtomValue } from "jotai";
 import React from "react";
-import { authInstance } from "src/lib/axios/instance";
+import { isSettledAuthAtom } from "./GlobalProvider";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      networkMode: process.env.NODE_ENV === "development" ? "always" : "online", // online is default
+      networkMode: process.env.NODE_ENV === "development" ? "always" : "online",
     },
   },
 });
@@ -17,13 +18,16 @@ export default function ReactQueryProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const isSettledAuth = useAtomValue(isSettledAuthAtom);
   queryClient.setDefaultOptions({
     queries: {
       ...queryClient.getDefaultOptions().queries,
-      enabled: !!authInstance.defaults.headers.common["Authorization"],
+      enabled: isSettledAuth,
     },
   });
   queryClient.setQueryDefaults(["getGroup"], { enabled: true });
+  queryClient.setQueryDefaults(["getNotifications"], { enabled: true });
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
