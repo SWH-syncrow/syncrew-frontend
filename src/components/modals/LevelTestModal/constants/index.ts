8d23df70095dc07267e16d929e307ac1116de4e3
runru1030 <runru1030@gmail.com>
2023-08-09 00:45:37 +0900

    hotfix : 시연 테스트 수정

--- src/components/modals/LevelTestModal/constants/index.ts
+++ src/components/modals/LevelTestModal/constants/index.ts
@@ -11,6 +11,6 @@ export const SW_ITEMS = {
 
 export const LEVEL = [
   { value: "초급", text: "디지털 기기와 아직 익숙하지 않은 상태예요!" },
-  { value: "증급", text: "디지털 기기와 어느정도 익숙한 수준이에요!" },
+  { value: "중급", text: "디지털 기기와 어느정도 익숙한 수준이에요!" },
   { value: "고급", text: "디지털 기기와 매우 친숙한 수준이에요!" },
 ];
